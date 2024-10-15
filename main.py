b3e2ddfdddb63d5a893a3b2fb1b0aebe7c285914
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-15 17:55:43 +0530

    detecting movement and adding rectangles

--- main.py
+++ main.py
@@ -14,8 +14,20 @@ while True:
         first_frame = grey_frame_gau
 
     delta_frame = cv.absdiff(first_frame, grey_frame_gau)
-    cv.imshow("my video", delta_frame)
 
+    thresh_frame = cv.threshold(delta_frame, 60, 255, cv.THRESH_BINARY)[1]
+    dilFrame = cv.dilate(thresh_frame, None, iterations=2)
+    cv.imshow("my video", dilFrame)
+
+    contours, check = cv.findContours(dilFrame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv.contourArea(contour) < 5000:
+            continue
+        x,y,w,h = cv.boundingRect(contour)
+        cv.rectangle(frame,(x,y),(x+w, y+h), (0,255,0), 3)
+
+    cv.imshow('video',frame) 
     key = cv.waitKey(1)
 
     if key == ord('q'):
