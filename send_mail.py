3835608642fe338b80aee77e6e45dc444182a21e
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-16 11:38:36 +0530

    added script to send email

--- send_mail.py
+++ send_mail.py
@@ -1,2 +1,27 @@
-def send_email():
-    print("Email is sent")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+SENDER = ''
+PASSWORD = ''
+RECEIVER = ''
+
+
+def send_email(img_path):
+    email_msg = EmailMessage
+    email_msg['Subject'] = "New Object Detected"
+    email_msg.set_content('New Object detected in webcame frame')
+
+    with open(img_path, 'rb') as file:
+        content = file.read()
+    email_msg.add_attachment(content, maintype='image', subtype=imghdr.what(None, content))
+    
+    gmail = smtplib.SMTP('smtp.gmail.com',587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER,PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_msg.as_string())
+    gmail.quit()
+
+if __name__ == '__main__':
+    send_email(img_path='images/test.png')
